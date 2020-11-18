ac268c4e33e03aeb03f2fc174411c49ba7417560
José M. Viñas <josemanuelvinhascid@gmail.com>
2020-11-18 17:31:21 +0100

    Corrección de la vista de paginación

--- view/home/index.php
+++ view/home/index.php
@@ -28,22 +28,24 @@ $page = $view->getVariable("page");
     include(__DIR__ . "/../layouts/videos_view.php");
     ?>
 
-    <div class="col-12 row justify-content-center align-items-center mt-2">
-        <?php if (isset($previous)): ?>
-            <a href="index.php?controller=home&action=index&page=<?= $previous ?>" class="m-2">
-                <img class="bt-pag m-2" src="static/img/anterior.svg" alt="<?= i18n("Previous") ?>">
-            </a>
-        <?php endif ?>
+    <?php if (isset($previous) || isset($next)): ?>
+        <div class="col-12 row justify-content-center align-items-center mt-2">
+            <?php if (isset($previous)): ?>
+                <a href="index.php?controller=home&action=index&page=<?= $previous ?>" class="m-2">
+                    <img class="bt-pag m-2" src="static/img/anterior.svg" alt="<?= i18n("Previous") ?>">
+                </a>
+            <?php endif ?>
 
-        <a href="index.php?controller=home&action=index&page=<?= $page ?>"
-           class="m-2"><?= $page ?></a>
+            <a href="index.php?controller=home&action=index&page=<?= $page ?>"
+               class="m-2"><?= $page ?></a>
 
-        <?php if (isset($next)): ?>
-            <a href="index.php?controller=home&action=index&page=<?= $next ?>" class="m-2">
-                <img class="bt-pag m-2" src="static/img/proximo.svg" alt="<?= i18n("Next") ?>">
-            </a>
-        <?php endif ?>
-    </div>
+            <?php if (isset($next)): ?>
+                <a href="index.php?controller=home&action=index&page=<?= $next ?>" class="m-2">
+                    <img class="bt-pag m-2" src="static/img/proximo.svg" alt="<?= i18n("Next") ?>">
+                </a>
+            <?php endif ?>
+        </div>
+    <?php endif ?>
 </div>
 
 
